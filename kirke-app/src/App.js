54bae7a3808980b6ab771e30649bb0dfdd62f989
js.sadhik <shaiksadhik0410@gmail.com>
2022-04-21 14:09:31 +0530

    02-rag-log-in-page-changes

--- kirke-app/src/App.js
+++ kirke-app/src/App.js
@@ -4,9 +4,7 @@ import LoginPage from './Components/Login/LoginPage';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <LoginPage />
-      </header>
+      <LoginPage />
     </div>
   );
 }
--- kirke-app/src/Components/Login/LoginPage.js
+++ kirke-app/src/Components/Login/LoginPage.js
@@ -1,61 +1,120 @@
-import React, { useState } from 'react';
-import './LoginPage.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-function LoginPage() {
-  const [data, setData] = useState({
-    email: '',
-    password: '',
-  });
+// function Copyright(props) {
+//   return (
+//     <Typography
+//       variant="body2"
+//       color="text.secondary"
+//       align="center"
+//       {...props}
+//     >
+//       {'Copyright © '}
+//       <Link color="inherit" href="https://mui.com/">
+//         Your Website
+//       </Link>{' '}
+//       {new Date().getFullYear()}
+//       {'.'}
+//     </Typography>
+//   );
+// }
 
-  const { email, password } = data;
+const theme = createTheme();
 
-  const inputHandler = (e) => {
-    setData({ ...data, [e.target.name]: e.target.value });
-    // console.log(data);
-  };
-  const submitHandler = (e) => {
-    e.preventDefault();
-    console.log(data);
+export default function SignIn() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      password: data.get('password'),
+    });
   };
 
   return (
-    <>
-      <div className="login_Container">
-        <h3>Login</h3>
-        <form className="mt-3 p-3" onSubmit={submitHandler}>
-          <div className="form-group ">
-            <label for="exampleInputEmail1">Email address</label>
-            <input
-              type="email"
+    <ThemeProvider theme={theme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
               name="email"
-              value={email}
-              className="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-              placeholder="Enter email"
-              onChange={inputHandler}
+              autoComplete="email"
+              autoFocus
             />
-          </div>
-          <div className="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input
-              type="password"
-              className="form-control"
+            <TextField
+              margin="normal"
+              required
+              fullWidth
               name="password"
-              value={password}
-              id="exampleInputPassword1"
-              placeholder="Password"
-              onChange={inputHandler}
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
             />
-          </div>
-          <button type="submit" className="btn btn-primary mt-4">
-            Login
-          </button>
-        </form>
-      </div>
-    </>
+            <FormControlLabel
+              control={<Checkbox value="remember" color="primary" />}
+              label="Remember me"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        {/* <Copyright sx={{ mt: 8, mb: 4 }} /> */}
+      </Container>
+    </ThemeProvider>
   );
 }
-
-export default LoginPage;
