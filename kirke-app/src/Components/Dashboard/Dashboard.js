1c35021b07c3c4a965b25d416ece3bdf9070ff94
Tushar Nile <tushar.nile@incedoinc.com>
2022-04-26 12:00:56 +0530

    Conflict resolved for Dashboard.js file

--- kirke-app/src/Components/Dashboard/Dashboard.js
+++ kirke-app/src/Components/Dashboard/Dashboard.js
@@ -2,22 +2,12 @@ import React, { useState, useRef, useEffect, useMemo, useCallback } from 'react'
 import { render } from 'react-dom';
 import "./Dashboard.css"
 import DatePicker from 'react-date-picker';
-<<<<<<< HEAD
-// import { AgGridReact } from 'ag-grid-react'; // the AG Grid React Component
-// import 'ag-grid-community/dist/styles/ag-grid.css'; // Core grid CSS, always needed
-// import 'ag-grid-community/dist/styles/ag-theme-alpine.css'; // Optional theme CSS
-
-import TextField from '@mui/material/TextField';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { DataGrid } from '@mui/x-data-grid';
-=======
 import { AgGridReact } from 'ag-grid-react'; // the AG Grid React Component
 import AuthContext from '../../context/AuthProvider';
 
 import axios from '../../api/axios';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../context/AuthProvider';
->>>>>>> 514cf21e6f329d628a326a2a61ce77f38512e42f
 
 
 const GETDATA_URL = "/getData";
@@ -32,67 +22,6 @@ const DashboardPage = (e) => {
     const navigate = useNavigate();
 
     const [value, onChange] = useState(new Date());
-<<<<<<< HEAD
-
-    const columns = [
-        { field: 'id', headerName: 'ID', width: 40 },
-        {
-            field: 'Program',
-            headerName: 'Program',
-            width: 150,
-            editable: true,
-        },
-        {
-            field: 'firstName',
-            headerName: 'First name',
-            width: 150,
-            editable: true,
-        },
-        {
-            field: 'lastName',
-            headerName: 'Last name',
-            width: 150,
-            editable: true,
-        },
-        {
-            field: 'DOJIncedo',
-            headerName: 'DOJ Incedo',
-            type: 'number',
-            width: 110,
-            editable: true,
-        },
-        {
-            field: 'CurrentLevel',
-            headerName: 'Current Level',
-            sortable: false,
-            width: 150,
-        },
-        {
-            field: 'LastPromotedOn',
-            headerName: 'Last Promoted On',
-            sortable: false,
-            width: 150,
-        },
-        {
-            field: 'CurrentManager',
-            headerName: 'Current Manager',
-            sortable: false,
-            width: 150,
-        },
-        {
-            field: 'RecordedOn',
-            headerName: 'Recorded On',
-            sortable: false,
-            width: 150,
-        },
-        {
-            field: 'RAG',
-            headerName: 'RAG',
-            sortable: false,
-            width: 150,
-        },
-    ];
-=======
     const gridRef = useRef(); // Optional - for accessing Grid's API
     
     const [rowData, setRowData] = useState(); // Set rowData to Array of Objects, one Object per Row
@@ -157,7 +86,6 @@ const DashboardPage = (e) => {
         { field: 'RAG', field: "RAG", width:150, cellClassRules: {
             'rag-green': 'x < 20'}  },
     ]);
->>>>>>> 514cf21e6f329d628a326a2a61ce77f38512e42f
 
     const rows = [
         { id: 1, Program: "Insurance", firstName: "Benn", lastName: "Tennyson", DOJIncedo: "4/Jan/2020", CurrentLevel: "3A", LastPromotedOn: "4/April/2022", CurrentManager: "James Sewer", RecordedOn: "24/April/2022", RAG: "" },
