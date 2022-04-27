6a268fe4720d3241ddf24a798de7479c88d975fb
Sandeep Kumar Jakkaraju <sandeepkumar.jakkaraju@gmail.com>
2022-04-27 16:47:18 +0530

    after grid

--- kirke-app/src/Components/Dashboard/Dashboard.js
+++ kirke-app/src/Components/Dashboard/Dashboard.js
@@ -10,15 +10,30 @@ import TextField from '@mui/material/TextField';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { DataGrid } from '@mui/x-data-grid';
 
+import AuthContext from '../../context/AuthProvider';
 
+import axios from '../../api/axios';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../context/AuthProvider';
+
+const GETDATA_URL = "/getData";
 
 const DashboardPage = () => {
     const [value, onChange] = useState(new Date());
 
+    const [user, setUser] = useState('');
+
+    const auth = useAuth();
+  
+    const navigate = useNavigate();
+
+    const [rows, setRows] = useState(''); // Set rowData to Array of Objects, one Object per Row
+
+
     const columns = [
         { field: 'id', headerName: 'ID', width: 40 },
         {
-            field: 'Program',
+            field: 'program',
             headerName: 'Program',
             width: 150,
             editable: true,
@@ -36,20 +51,20 @@ const DashboardPage = () => {
             editable: true,
         },
         {
-            field: 'DOJIncedo',
+            field: 'doj',
             headerName: 'DOJ Incedo',
             type: 'number',
             width: 110,
             editable: true,
         },
         {
-            field: 'CurrentLevel',
+            field: 'currentLevel',
             headerName: 'Current Level',
             sortable: false,
             width: 150,
         },
         {
-            field: 'LastPromotedOn',
+            field: 'lastPromotedOn',
             headerName: 'Last Promoted On',
             sortable: false,
             width: 150,
@@ -67,20 +82,62 @@ const DashboardPage = () => {
             width: 150,
         },
         {
-            field: 'RAG',
+            field: 'rag',
             headerName: 'RAG',
             sortable: false,
             width: 150,
         },
     ];
 
-    const rows = [
-        { id: 1, Program: "Insurance", firstName: "Benn", lastName: "Tennyson", DOJIncedo: "4/Jan/2020", CurrentLevel: "3A", LastPromotedOn: "4/April/2022", CurrentManager: "James Sewer", RecordedOn: "24/April/2022", RAG: "" },
-
-    ];
-    //       const BasicDatePicker = () => {
-    //         const [value, setValue] = React.useState(null)
-    //    }
+    useEffect(()=>{
+      
+        try {
+    
+            const reponse = axios.post(GETDATA_URL, JSON.stringify({
+             month: 3,
+             year: 2022
+            }), {
+              headers: { 'Content-Type': 'application/json' },
+              withCredentials: false
+            }
+            );
+      
+            reponse.then(function (result) {
+              console.log(result);
+              console.log(JSON.stringify(result.data));
+      
+              console.log(result.data.status)
+              if (result.data.status === "success"){      
+                setRows(result.data.ragEntryList);
+              }
+              else{
+                navigate('/error');
+              }  
+            }).catch(function (error) {
+              if (error.response) {
+                // Request made and server responded
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+              } else if (error.request) {
+                // The request was made but no response was received
+                console.log(error.request);
+                navigate('/error');
+              } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error', error.message);
+              }
+          
+            });
+      
+      
+          } catch (err) {
+            console.log(err);
+            navigate('/');
+          }
+        }, [])
+    
+     
     return (
         <div className='DashboardMainDiv'>
             <div className='row'>
